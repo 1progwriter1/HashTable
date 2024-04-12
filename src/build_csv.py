f0d852cee21bde3e1595567c963b114d4319ced4
1progwriter1 <vig130205@mail.ru>
2024-04-12 13:42:11 +0300

    check font size

--- src/build_csv.py
+++ src/build_csv.py
@@ -9,13 +9,13 @@ data = pd.read_csv("Results/hash_tables/hash_1.csv")
 plt.xlim([-10, max(data['index']) + 100])
 plt.ylim([0, max(data['size']) + 100])
 
-plt.xticks(range(0, max(data['index']) + 10, 100))
-plt.yticks(range(0, max(data['size']) + 10, 500))
+plt.xticks(range(0, max(data['index']) + 10, 200), size=20)
+plt.yticks(range(0, max(data['size']) + 10, 500), size=20)
 
 plt.bar(data['index'], data['size'], color='red')
 
-plt.xlabel("index")
-plt.ylabel("size")
+plt.xlabel("index", size=30)
+plt.ylabel("size", size=30)
 plt.title("return 0", size=30)
 
 plt.savefig("Images/charts/hash_func_1.png")
